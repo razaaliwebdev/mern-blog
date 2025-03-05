95f95817732912d7f51689349a9266467f243b8b
razaaliwebdev <razaali.webdev@gmail.com>
2025-03-06 04:16:53 +0500

    create and run the server

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Sever is running on the port:${PORT}`);
+});
